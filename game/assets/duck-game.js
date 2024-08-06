a6c126e5c4908520466861203ae00b25cb2c5148
Travis Wade <travis@twade.io>
2024-08-06 17:06:23 -0400

    new bird counter

--- game/assets/duck-game.js
+++ game/assets/duck-game.js
@@ -6,13 +6,15 @@ const miniDucks = [
     document.getElementById('miniDuck4'),
     document.getElementById('miniDuck5')
 ];
-const counter = document.getElementById('counter');
+const eggCounter = document.getElementById('eggCounter');
+const birdCounter = document.getElementById('birdCounter');
 const popSound = document.getElementById('popSound');
 const bgMusic = document.getElementById('bgMusic');
 const bird = document.getElementById('bird');
 const birdSound = document.getElementById('birdSound');
 const chirpSound = new Audio('assets/chirp-chirp.mp3'); // Load the chirp sound
 let eggCount = 0;
+let birdCount = 0;
 let mouseTimeout;
 let wanderingInterval;
 
@@ -62,7 +64,7 @@ function placeEgg() {
     egg.addEventListener('mouseenter', () => {
         egg.remove();
         eggCount++;
-        counter.textContent = `Eggs: ${eggCount}`;
+        eggCounter.textContent = `Eggs: ${eggCount}`;
         popSound.currentTime = 0; // Rewind to the start
         popSound.play();
     });
@@ -163,6 +165,8 @@ document.addEventListener('mousemove', () => {
         birdRect.bottom > cursorDuckRect.top
     ) {
         bird.style.display = 'none';
+        birdCount++;
+        birdCounter.textContent = `Birds: ${birdCount}`;
         chirpSound.currentTime = 0;
         chirpSound.play();
     }
