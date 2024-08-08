65ed443445c680eb00b49acce0d9dd2b94272b27
Travis Wade <travis@twade.io>
2024-08-07 21:11:15 -0400

    new menu close behavior

--- assets/menu.js
+++ assets/menu.js
@@ -1,16 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.getElementById('menuIcon');
     const menu = document.getElementById('menu');
+    const menuContent = document.querySelector('.menu-content');
     let menuOpen = false;
 
     menuIcon.addEventListener('click', function() {
         if (menuOpen) {
-            menu.classList.remove('open');
-            menuIcon.src = 'assets/menu-open.png';
+            closeMenu();
         } else {
-            menu.classList.add('open');
-            menuIcon.src = 'assets/menu-close.png';
+            openMenu();
         }
-        menuOpen = !menuOpen;
     });
+
+    menu.addEventListener('click', function(event) {
+        if (!menuContent.contains(event.target)) {
+            closeMenu();
+        }
+    });
+
+    function openMenu() {
+        menu.classList.add('open');
+        menuIcon.src = 'assets/menu-close.png';
+        menuOpen = true;
+    }
+
+    function closeMenu() {
+        menu.classList.remove('open');
+        menuIcon.src = 'assets/menu-open.png';
+        menuOpen = false;
+    }
 });
