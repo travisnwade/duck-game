c225c09b1ed7d1a460f780de2c74d6c9e47cc9de
Travis Wade <travis@twade.io>
2024-08-11 06:15:29 -0400

    minify, cleanup

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,35 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.getElementById('menuIcon');
-    const menu = document.getElementById('menu');
-    const body = document.body;
-    let menuOpen = false;
+	const menuIcon = document.getElementById('menuIcon');
+	const menu = document.getElementById('menu');
+	const body = document.body;
+	let menuOpen = false;
 
-    menuIcon.addEventListener('click', function() {
-        if (menuOpen) {
-            closeMenu();
-        } else {
-            openMenu();
-        }
-    });
+	menuIcon.addEventListener('click', function() {
+		if (menuOpen) {
+			closeMenu();
+		} else {
+			openMenu();
+		}
+	});
 
-    menu.addEventListener('click', function(event) {
-        const menuContent = document.querySelector('.menu-content');
-        if (!menuContent.contains(event.target)) {
-            closeMenu();
-        }
-    });
+	menu.addEventListener('click', function(event) {
+		const menuContent = document.querySelector('.menu-content');
+		if (!menuContent.contains(event.target)) {
+			closeMenu();
+		}
+	});
 
-    function openMenu() {
-        menu.classList.add('open');
-        menuIcon.src = 'assets/images/menu-close.png';
-        body.style.cursor = 'auto'; // Ensure normal cursor when menu is open
-        menuOpen = true;
-    }
+	function openMenu() {
+		menu.classList.add('open');
+		menuIcon.src = 'assets/images/menu-close.png';
+		body.style.cursor = 'auto'; // Ensure normal cursor when menu is open
+		menuOpen = true;
+	}
 
-    function closeMenu() {
-        menu.classList.remove('open');
-        menuIcon.src = 'assets/images/menu-open.png';
-        body.style.cursor = 'auto'; // Ensure normal cursor when menu is closed
-        menuOpen = false;
-    }
+	function closeMenu() {
+		menu.classList.remove('open');
+		menuIcon.src = 'assets/images/menu-open.png';
+		body.style.cursor = 'auto'; // Ensure normal cursor when menu is closed
+		menuOpen = false;
+	}
 });
